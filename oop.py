efd12d0848564e21ab4d61b7f469339677df7147
Suhail Azhr <suhailazhr01@gmail.com>
2026-08-05 16:52:53 +0530

    add class

--- oop.py
+++ oop.py
@@ -11,7 +11,7 @@ print(stu1.name , stu1.subject, stu1.cgpa)
 
 
 #constractor==> init = to initialise the class it is call to build a object
- 
+'''
 class student:
     def __init__(self , name, cgpa):
         self.name = name
@@ -27,5 +27,42 @@ print(stu1.name , stu1.cgpa)
 print(stu2.name , stu2.cgpa)
 print(stu3.name , stu3.cgpa)
 print(stu4.name , stu4.cgpa)
+
+
+'''
+
+class student:
+    college_name = "A B C" #class attributes
+    PI = 3.1
+
+    '''def __init__(self): #default construtor
+        print("Obj is being counstructor..")
+'''
+#constructor == has two ==> default and parameterized
+
+    def __init__(self,name,cgpa): #parameterized constructor
+        self.name = name  #instance attributes
+        self.cgpa = cgpa
+        self.PI = 3.14
+
+    def get_cgpa(self):
+        return self.cgpa
+
+stu1 = student("Suhail" , 9.0)
+
+
+
+#stu1.get_cgpa()
+print(f"{stu1.name} has cgpa = {stu1.get_cgpa()}")
+
+print(stu1.name) #call instance attribure
+
+print(student.college_name) #call class attribute
+
+print(stu1.PI) #high priority is that take the value from instance attribute sa
+
+print(student.PI) #but when we call the class attribute take the value from class attribute
+
+
 
 
