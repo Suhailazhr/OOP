bfa99fdf4e6870387118dacf9b35c2c7d1a340f6
Suhail Azhr <suhailazhr01@gmail.com>
2026-08-08 19:21:36 +0530

    update

--- oop.py
+++ oop.py
@@ -64,6 +64,7 @@ print(stu1.PI) #high priority is that take the value from instance attribute sa
 print(student.PI) #but when we call the class attribute take the value from class attribute
 
 '''
+'''
 class laptop:
     storage_type = "Ssd"
 
@@ -88,4 +89,62 @@ l1 = laptop("16RAM","512GB")
 l1.get_info()
 
 laptop.get_storage_type()
-l1.cal_discount(40_000,11)
+l1.cal_discount(40_000,11)'''
+
+# oop are four piller 
+#one is encapsulation
+'''
+class Bankaccount:
+
+    def __init__(self,name,balance):
+        self.name = name #public attribute
+        self.__balance = balance #data mangling
+
+        #use getter and setter
+    def get_balance(self): #getter
+        return self.__balance
+
+    def set_balance(self,newBalance):
+        self.__balance = newBalance  
+
+acc1 = Bankaccount("Suhail", 100_000)
+acc1.set_balance(200_000)
+print(acc1.name,acc1._Bankaccount__balance)'''
+
+#second is inheritance
+'''
+class Employe:
+        start_time = "10AM"
+        end_time = "6PM"
+    
+class Adminstaff(Employe):
+        def __init__(self,role):
+            self.role = role
+
+class Accountant(Adminstaff):
+        def __init__(self, salary,role):
+            super().__init__(role)
+            self.salary = salary
+
+
+acc1 = Accountant(25_000, "CA")
+
+print(acc1.role,acc1.salary, acc1.start_time,acc1.end_time)'''
+
+class Teacher:
+    def __init__(self,salary):
+        self.salary = salary
+
+class Student:
+    def __init__(self,gpa):
+        self.gpa = gpa
+
+class TA(Teacher, Student):
+    def __init__(self,salary,gpa,name):
+        super().__init__(salary)
+        Student.__init__(self,gpa)
+        self.name = name
+
+t1 = TA(25_000, 9.0, "Suhail")
+
+print(t1.name,t1.salary,t1.gpa)
